17a0da6d6d395dc70a2c9348154523060c03b2d4
Arzu Caner <arzuguneycaner@gmail.com>
2025-03-09 19:41:51 +0000

    Updated App.js structure and routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar"; 
+import Home from "./pages/Home/Home";
+import Learn from "./pages/Learn/Learn";
+import Scenario from "./pages/Scenario/Scenario";
+import Quiz from "./pages/Quiz/Quiz";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar /> 
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/learn" element={<Learn />} />
+        <Route path="/scenario" element={<Scenario />} />
+        <Route path="/quiz" element={<Quiz />} />     
+      </Routes>
+
+      <Footer />
+    </Router>
   );
 }
 
 export default App;
+
+
